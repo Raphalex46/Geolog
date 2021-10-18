4d02fb2488d4e8baef0b491f870da4478ae1daec
Raphaël Colin <raphael.colin68@gmail.com>
2021-10-18 23:09:07 +0200

    Initial Commit: Create load file for command line options

---
+++ src/load.pl
@@ -0,0 +1,59 @@
+:- module(load, [parse_command_line/2, assert_options/1, option/1, version/1]).
+:- use_module(library(optparse)).
+
+% version(Version).
+version('0.1.0').
+
+% option(Option).
+% Option is a term representing a command line option
+% (load_option is used to be able to declare the 'option' predicate in the module
+% declaration while being able to assert options in the assert_options predicate).
+option(X) :- load_option(X).
+
+% List of available options
+optspecs(
+  [
+    [
+      % Name of output file
+      opt(output_file),
+      type(atom),
+      shortflags([o]),
+      longflags([output]),
+      default(out),
+      help(['output file name'])
+    ],
+    [
+      % Language selection
+      opt(lang),
+      type(atom),
+      shortflags([l]),
+      longflags([lang]),
+      default(english),
+      help('Natural language selection')
+    ],
+    [
+      opt(version),
+      type(boolean),
+      shortflags([v]),
+      longflags([version]),
+      default(false),
+      help('Print version number and exit')
+    ]
+  ]
+).
+
+% parse_command_line(Opts, PositionalArgs).
+% Parse the command line to get a list of options and positional arguments
+parse_command_line(Opts, PositionalArgs) :-
+  % Get our option specification and the cli input (argv)
+  optspecs(OptSpec),
+  current_prolog_flag(argv, Argv),
+  opt_parse(OptSpec, Argv, Opts, PositionalArgs).
+
+% assert_options(Opts).
+% This predicate asserts all options passed in the command line
+% as terms of the form option(Opt)
+assert_options([]).
+assert_options([HeadOpt | Opts]) :-
+  assert(load_option(HeadOpt)),
+  assert_options(Opts).
