f51f743531aed9688faaaaa63dd15270bb530174
Raphaël Colin <raphael.colin68@gmail.com>
2021-10-19 11:24:11 +0200

    First part of lexer.

--- src/tokens.pl
+++ src/tokens.pl
@@ -11,7 +11,25 @@ i18n(french, let, soit).
 % The token predicate converts input tokens into tokens for the parser
 
 % Keywords
+% This predicate is used to distinguish between keywords and identifiers
+keyword(let).
+
+% Ponctuation
+punctuation(',').
+punctuation('.').
+
+% Actual token predicates
+% Those are a green cuts because of the body of 'token(Id, ident(Id))'.
+
+% Keywords
 token(let, let) :- !.
 
+% Punctuation
+token('.', '.') :- !.
+token(',', ',') :- !.
+
 % Identifiers
-token(Id, ident(Id)).
+token(Id, ident(Id)) :-
+  % Id is an identifier only if it isn't also a keyword (otherwise it's a keyword)
+  \+ keyword(Id),
+  \+ punctuation(Id).
