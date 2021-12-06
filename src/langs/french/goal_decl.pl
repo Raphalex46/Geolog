498a2f905051e026dc73e539a159fff0557994bd
Raphaël Colin <raphael.colin68@gmail.com>
2021-12-06 16:42:08 +0100

    Add new french goal declarations

--- src/langs/french/goal_decl.pl
+++ src/langs/french/goal_decl.pl
@@ -5,3 +5,11 @@
 
 goal_decl(NameList, Type) --> [trouver], name_list(NameList), [de, type],
 typename(Type), [tel, que].
+
+% Singular
+goal_decl([Name], Type) --> [trouver], typename(Type), [ident(Name),
+tel, que].
+
+% Plural
+goal_decl(NameList, Type) --> [trouver], plural_typename(Type),
+name_list(NameList), [tels, que].
